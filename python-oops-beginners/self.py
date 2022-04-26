586a013d42abed4c2459d82d7834f9664cb27cca
Jeremy Hancock <talktojer@gmail.com>
2022-04-26 17:28:32 -0400

    self.py lesson

---
+++ python-oops-beginners/self.py
@@ -0,0 +1,11 @@
+class Employee:
+    def employeeDetails(self):
+        self.name = "Matthew"
+        print("Name:", self.name)
+        employee.name = self.name
+
+
+employee = Employee()
+employee.employeeDetails()
+name = employee.employeeDetails()
+#print(employee.name)
